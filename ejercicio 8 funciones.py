6f6f50fb80e1f872954af1581197d74e9cd5d85c
Giovanny Rueda <giovannirueda588@gmail.com>
2025-12-11 16:16:52 -0500

    Update ejercicio 8 funciones.py

--- ejercicio 8 funciones.py
+++ ejercicio 8 funciones.py
@@ -24,3 +24,26 @@ def mostrar_reporte(total_personas, total_edades, objetivo):
     print(f"Total participantes: {total_personas}")
     print(f"Promedio de edad: {promedio:.2f}")
     print(f"Público objetivo (25-45 años): {objetivo}")
+#******codigo principal*****
+
+total_personas = 0
+suma_edades = 0
+publico_objetivo = 0
+
+while True:
+    edad = solicitar_edad()
+
+    if edad == 0:
+        break
+
+    total_personas += 1
+    suma_edades += edad
+
+    if pertenece_rango(edad):
+        publico_objetivo = registrar_objetivo(publico_objetivo)
+    else:
+        print("Edad registrada fuera del rango objetivo.")
+
+mostrar_reporte(total_personas, suma_edades, publico_objetivo)
+
+
